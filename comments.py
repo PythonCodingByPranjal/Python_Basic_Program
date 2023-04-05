c8e6cd20b68f8ef34def069a9df10d2d25acc336
Pranjal8983 <pranjaldeka8983@gmail.com>
2023-04-05 12:57:41 +0530

    multi line comments

--- comments.py
+++ comments.py
@@ -1,2 +1,3 @@
 print("comments are nothing but to understand the script you can note some information")
 print("comments in python are declared using # symbol")
+print("Multiline comments are defined using three times single quotation ''' or double quotation")
