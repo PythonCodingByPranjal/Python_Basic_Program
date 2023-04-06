f2f46899b1701e5343d46d5da2dc1ed7f3657845
Pranjal8983 <pranjaldeka8983@gmail.com>
2023-04-06 11:07:27 +0530

    square and boolean

---
+++ boolean.py
@@ -0,0 +1,7 @@
+# Boolean type example
+value1 = True
+print(value1)
+
+#number to boolean convert
+number = 12
+print(bool(number))
---
+++ square.py
@@ -0,0 +1,17 @@
+import math
+# Assign values to x and n
+x = 2
+n = 3
+# Method 1
+power = x ** n
+print("%d to the power %d is %d" % (x, n, power))
+'''
+import math
+# Assign values to x and n
+x = 4
+n = 3
+
+# Method 1
+power = x ** n
+print("%d to the power %d is %d" % (x, n, power))
+'''
