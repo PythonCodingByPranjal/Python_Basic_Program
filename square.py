c5ce2f66032d4f74e53b1a17211ecadcc2a3b692
Pranjal8983 <pranjaldeka8983@gmail.com>
2023-04-06 11:09:23 +0530

    square second commit

--- square.py
+++ square.py
@@ -5,13 +5,3 @@ n = 3
 # Method 1
 power = x ** n
 print("%d to the power %d is %d" % (x, n, power))
-'''
-import math
-# Assign values to x and n
-x = 4
-n = 3
-
-# Method 1
-power = x ** n
-print("%d to the power %d is %d" % (x, n, power))
-'''
